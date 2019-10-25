cdb37170d1b39c16a6be9f4a364aecbaf8440487
Jean Souza <jeancfsouza@gmail.com>
2019-10-24 21:26:01 -0300

    Initial commit

---
+++ packages/master-of-bums/src/main/java/br/com/junkkeeper/masterofbums/masterofbums/MasterOfBumsApplication.java
@@ -0,0 +1,13 @@
+package br.com.junkkeeper.masterofbums.masterofbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterOfBumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterOfBumsApplication.class, args);
+	}
+
+}
---
+++ packages/master-of-bums/src/test/java/br/com/junkkeeper/masterofbums/masterofbums/MasterOfBumsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.junkkeeper.masterofbums.masterofbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterOfBumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
